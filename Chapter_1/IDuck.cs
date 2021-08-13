29d74e707a474c31523f7e18782177734e20050b
Zeke Juel <Zeke.Juel@inclusa.org>
2021-08-13 11:43:32 -0500

    adding Ducks

---
+++ Chapter_1/IDuck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeadFirst_DesignPatterns.Chapter_1
+{
+    public interface IDuck
+    {
+        public string StrDisplay
+        {
+            get; set;
+        }
+        public string StrFly
+        {
+            get; set;
+        }
+        public string StrQuack
+        {
+            get; set;
+        }
+        public string StrSwim
+        {
+            get; set;
+        }
+        string Display();
+        string Fly();
+        string Quack();
+        string Swim();
+        //Other duck-like methods...
+    }
+}
--- Chapter_1/SimUDuck.cs
+++ Chapter_1/SimUDuck.cs
@@ -19,7 +19,91 @@ namespace HeadFirst_DesignPatterns.Chapter_1
 
         public int RunDemo()
         {
-            throw new NotImplementedException();
+            List<Duck> ducks = new List<Duck>();
+            ducks.Add(new Duck());
+            Console.WriteLine("");
+            ducks.Add(new MallardDuck());
+            Console.WriteLine("");
+            ducks.Add(new RedheadDuck());
+            Console.WriteLine("");
+            ducks.Add(new RubberDuck());
+            Console.WriteLine("");
+
+            foreach (Duck duck in ducks)
+            {
+                duck.printDuckInfo();
+            }
+
+            return 0;
+        }
+    }
+
+    class Duck : IDuck
+    {
+        public string StrDisplay { get; set; }
+        public string StrFly { get; set; }
+        public string StrQuack { get; set; }
+        public string StrSwim { get; set; }
+
+        public Duck()
+        {
+            Console.Write("init Duck ");
+            StrDisplay = "I am a duck";
+            StrFly = "I can fly";
+            StrSwim = "I can swim";
+            StrQuack = "I can quack";
+        }
+        public string Display()
+        {
+            return StrDisplay;
+        }
+
+        public string Fly()
+        { 
+            return StrFly;
+        }
+        public string Swim()
+        {
+            return StrSwim;
+        }
+
+        public string Quack()
+        {
+            return StrQuack;
+        }
+        public void printDuckInfo()
+        {
+
+            Console.WriteLine($"\n{this.Display()}\n{this.Fly()}\n{this.Swim()}\n{this.Quack()}");
+        }
+
+    }
+
+    class MallardDuck : Duck
+    {
+        public MallardDuck() : base()
+        {
+            Console.Write("init MallardDuck ");
+            StrDisplay = "I'm a mallard";
+        }
+    }
+
+    class RedheadDuck : Duck
+    {
+        public RedheadDuck()
+        {
+            Console.Write("init RedheadDuck ");
+            StrDisplay = "I'm a red headed duck";
+        }
+    }
+    class RubberDuck : Duck
+    {
+        public RubberDuck() : base()
+        {
+            Console.Write("init RubberDuck ");
+            StrDisplay = "I'm a rubber duck";
+            StrQuack = "I can squeak";
+            StrFly = "I can't fly";
         }
     }
 }
